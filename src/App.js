4c4d10f28b487067dcdc7899347f6c9a8b15ccc1
Dark.Hades <dark.hades.1102@gmail.com>
2019-02-11 00:26:34 +0900

    Page Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
 
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Sider from './components/Footer';
 
 import './App.css';
 import 'antd/dist/antd.css';
@@ -12,11 +14,22 @@ class App extends Component
   render()
   {
     return (
-      <div className="App">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <Layout>
+        <Layout.Header style={{ height:'inherit',lineHeight:'inherit' }}>
+          <Header />
+        </Layout.Header>
+        <Layout>
+          <Layout.Sider>
+            <Sider />
+          </Layout.Sider>
+          <Layout.Content>
+            <Main />
+          </Layout.Content>
+        </Layout>
+        <Layout.Footer>
+          <Footer />
+        </Layout.Footer>
+      </Layout>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,42 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import {
+  Link, withRouter
+} from 'react-router-dom';
 import { Menu, Icon } from 'antd';
 
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+// const SubMenu = Menu.SubMenu;
+// const MenuItemGroup = Menu.ItemGroup;
 
-class Header extends Component
+class _Header extends Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    // Don't call this.setState() here!
+    this.state = {
+    };
+  }
+
   render()
   {
     return (
-      <header>
-        <Menu
-          // onClick={this.handleClick}
-          // selectedKeys={[this.state.current]}
-          mode="horizontal"
-        >
-          <Menu.Item key="mail">
-            <Icon type="mail" />Navigation One
-          </Menu.Item>
-          <Menu.Item key="app" disabled>
-            <Icon type="appstore" />Navigation Two
-          </Menu.Item>
-          <SubMenu
-            title={
-              <span className="submenu-title-wrapper">
-                <Icon type="setting" />
-                Navigation Three - Submenu
-              </span>
-            }
-          >
-            <MenuItemGroup title="Item 1">
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </MenuItemGroup>
-            <MenuItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </MenuItemGroup>
-          </SubMenu>
-          <Menu.Item key="alipay">
-            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-          </Menu.Item>
-        </Menu>
-      </header>
+      <Menu
+        theme="dark"
+        selectedKeys={[this.props.location.pathname]}
+        mode="horizontal"
+      >
+        <Menu.Item key="/">
+          <Link to="/">
+            <Icon type="home" />Home
+          </Link>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
+
+const Header = withRouter(_Header);
 
 export default Header;
 

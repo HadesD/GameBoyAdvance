47a8a6c69c7d68b8aa78500aff9a638f7ae55d9c
Dark.Hades <dark.hades.1102@gmail.com>
2019-02-12 03:22:31 +0900

    Add css

--- src/components/Play.js
+++ src/components/Play.js
@@ -4,24 +4,22 @@ import BodyScrollUnAble from './BodyScrollUnAble';
 import emuBKG from '../images/emu-background.svg';
 import dpadIcon from '../images/dpad.svg';
 
+import './Play.css';
+
 class Play extends BodyScrollUnAble
 {
   render()
   {
     const dPadSize = 100;
     return (
-      <div style={{ backgroundImage: 'url('+emuBKG+')', height: 'calc(100vh - 25px)', touchAction: 'manipulation', }}>
+      <div style={{ backgroundImage: 'url('+emuBKG+')', height: 'calc(100vh - 25px)', touchAction: 'manipulation', }} className="noselect">
         <button
           type="button"
+          className="console-btn"
           style={{
             backgroundImage: 'url('+dpadIcon+')',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: 'transparent',
-            backgroundSize: 'cover',
             width: dPadSize + 'px',
             height: dPadSize + 'px',
-            border: 'none',
-            outline: 'none',
           }}
         >
         </button>

25a9a338b3931516652e30892f7761ac3f664a34
Dark.Hades <dark.hades.1102@gmail.com>
2019-02-15 19:01:44 +0900

    Use unzip lib

--- src/components/play/Emulator.js
+++ src/components/play/Emulator.js
@@ -13,11 +13,14 @@ class Emulator extends React.Component
 
     this.parent = props.parent;
     this.screenRef = React.createRef();
+    this.inputFileRef = React.createRef();
 
     this.emulatorManager = new EmulatorManager(this);
 
+    this.onClick = this.onClick.bind(this);
     this.onDrop = this.onDrop.bind(this);
     this.onDragOver = this.onDragOver.bind(this);
+    this.inputFileOnChange = this.inputFileOnChange.bind(this);
   }
 
   componentDidMount()
@@ -44,10 +47,26 @@ class Emulator extends React.Component
     const file = e.dataTransfer.files[0];
     if (!file)
     {
-      alert('File Object is empty');
       return;
     }
 
+    this.emulatorManager.loadRomFile(file);
+  }
+
+  onClick(e)
+  {
+    e.preventDefault();
+    this.inputFileRef.current.click();
+  }
+
+  inputFileOnChange(e)
+  {
+    e.preventDefault();
+    const file = e.target.files[0];
+    if (!file)
+    {
+      return;
+    }
     this.emulatorManager.loadRomFile(file);
   }
 
@@ -60,7 +79,21 @@ class Emulator extends React.Component
           backgroundImage: 'url('+emuCanvasBKG+')',
         }}
       >
-        <canvas ref={this.screenRef} onDrop={this.onDrop} onDragOver={this.onDragOver} />
+        <canvas
+          ref={this.screenRef}
+          onClick={this.onClick}
+          onDrop={this.onDrop}
+          onDragOver={this.onDragOver}
+        />
+        <input
+          ref={this.inputFileRef}
+          type="file"
+          accept=".gb,.gba,.gbc,.zip"
+          style={{
+            display: 'none',
+          }}
+          onChange={this.inputFileOnChange}
+        />
       </div>
     );
   }
--- src/components/play/EmulatorManager.js
+++ src/components/play/EmulatorManager.js
@@ -1,9 +1,14 @@
 import GameBoy from './lib/amebo';
+import { Zlib } from 'zlibjs/bin/unzip.min';
 
 class EmulatorManager
 {
   gbApi = null;
   gbaApi = null;
+
+  currentRomFileName = null; // The file name of rom
+  currentRomName = null; // Name in ROM OFFSET
+  currentRomGameType = null; // GBA/GB/GBC
 
   constructor(parent)
   {
@@ -51,24 +56,49 @@ class EmulatorManager
 
   loadRomUrl(url)
   {
+    const hRequest = new XMLHttpRequest();
+    hRequest.open('GET', url);
+    hRequest.onload = function(e) {
+      console.log(e);
+    };
+    hRequest.send();
   }
 
   // Like input.type="file" / drag|drop.file
   loadRomFile(fileObj)
   {
     const self = this;
-    let reader = new FileReader();
-    reader.onload = function(e) {
-      // let arrayBuffer = e.result;
-      console.log(e.target.result);
-      self.loadRomBuffer(e.target.result);
-      // let array = new Uint8Array(arrayBuffer);
-        // binaryString = String.fromCharCode.apply(null, array);
-
-      // console.log(binaryString);
-    }
+    const reader = new FileReader();
+    reader.addEventListener('load', function(e) {
+      console.log(fileObj.name);
+      const buffer = e.target.result;
+      if (fileObj.name.indexOf('.zip') !== -1)
+      {
+        self.loadRomZipFile(buffer);
+        return;
+      }
+      this.loadRomBuffer(buffer);
+    });
     console.log(fileObj);
     reader.readAsArrayBuffer(fileObj);
+  }
+
+  // Buffer is Zip file ArrayBuffer
+  loadRomZipFile(buffer)
+  {
+    console.log(Zlib);
+    const unzip = new Zlib.Unzip(new Buffer(buffer));
+    const filenames = unzip.getFilenames();
+    for (let i = 0; i < filenames.length; i++)
+    {
+      const filename = filenames[i];
+      if (filename.indexOf('.gb') !== -1)
+      {
+        this.loadRomBuffer(new Uint8Array(unzip.decompress(filename)));
+        break;
+      }
+    }
+    console.log(filenames);
   }
 }
 
